d4e5d61f490360b7388c0b662f3c07fe02ed52a3
Muhammad waheed <162149796+Muhammadwaheedaree@users.noreply.github.com>
2024-04-21 13:23:55 +0500

    Add files via upload

---
+++ DAY8/Question22.js
@@ -0,0 +1,4 @@
+var friends = ["perin", "adeel", "naveed"];
+console.log(friends[3]); //Intentional error: Arrays are zero-indexed,so index 3 is out of bounds.
+friends[2];
+("naveed"); // correcting the error by accessing a valid index.
---
+++ DAY8/Question22.ts
@@ -0,0 +1,4 @@
+let friends: string[] = ["perin", "adeel", "naveed"];
+console.log(friends[3]); //Intentional error: Arrays are zero-indexed,so index 3 is out of bounds.
+friends[2];
+("naveed"); // correcting the error by accessing a valid index.
