3d6d84b1029e68a74839fc33d37c49344f7f3b42
Muhammad waheed <162149796+Muhammadwaheedaree@users.noreply.github.com>
2024-04-22 20:51:54 +0500

    Add files via upload

---
+++ DAY11/Question31.js
@@ -0,0 +1,7 @@
+var username = [];
+if (username.length === 0) {
+    console.log("we need find some users!");
+}
+else {
+    //Greate users
+}
---
+++ DAY11/Question31.ts
@@ -0,0 +1,6 @@
+let username: string[] = [];
+if (username.length === 0) {
+  console.log("we need find some users!");
+} else {
+  //Greate users
+}
