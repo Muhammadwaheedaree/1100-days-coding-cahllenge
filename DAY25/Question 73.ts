63c4e51d63ccd04ea94f1e987998ce00de8151c0
Muhammad waheed <162149796+Muhammadwaheedaree@users.noreply.github.com>
2024-04-28 08:57:08 +0500

    Add files via upload

---
+++ DAY25/Question 73.ts
@@ -0,0 +1,8 @@
+// Assigning and Updating Variable:
+function updateVariable() {
+  let number = 10;
+  console.log("Initial value:", number);
+  number = 20;
+  console.log("Updated value:", number);
+}
+updateVariable();
---
+++ DAY25/Question 74.js
@@ -0,0 +1,10 @@
+// Swapping Variable:
+function swapValues() {
+    var a = 5, b = 10;
+    console.log("Before swap:a=", a, "b=", b);
+    var temp = a;
+    a = b;
+    b = temp;
+    console.log("After swap: a=", a, "b=", b);
+}
+swapValues();
