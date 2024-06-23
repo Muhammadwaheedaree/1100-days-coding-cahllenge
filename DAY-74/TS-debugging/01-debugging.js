04afab06a3e2fe6f506ba37bc3401a419abf5b63
Muhammad waheed <162149796+muhammadwaheedaree@users.noreply.github.com>
2024-06-23 07:09:01 -0700

    Delete DAY-74/TS-debugging directory

--- DAY-74/TS-debugging/01-debugging.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-const user = 'World';
-console.log(greet(user));
-//# sourceMappingURL=01-debugging.js.map
