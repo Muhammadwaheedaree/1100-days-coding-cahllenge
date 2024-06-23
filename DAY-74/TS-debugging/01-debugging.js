52a3763f543d08bf734456643ab2f27cdc435d6f
Muhammad waheed <162149796+muhammadwaheedaree@users.noreply.github.com>
2024-06-23 19:10:06 +0500

    Add files via upload

---
+++ DAY-74/TS-debugging/01-debugging.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const user = 'World';
+console.log(greet(user));
+//# sourceMappingURL=01-debugging.js.map
